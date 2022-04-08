0081f14e2424ae0d4615cc5b9a92d96e40cbf275
MSSkowron <92554920+MSSkowron@users.noreply.github.com>
2022-04-08 22:06:38 +0200

    Add files via upload

---
+++ lab3/fraction.h
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#ifndef FRACTION_H
+#define FRACTION_H
+
+class Fraction
+{
+#ifndef _MSC_FULL_VER // if not Visual Studio Compiler
+#else
+    #pragma message ("Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym")
+#endif
+private:
+    void simplify();
+    int numerator_;
+    int denominator_;
+public:
+    explicit Fraction(int n = 0, int d = 1): numerator_{n}
+    {
+        if (d == 0) {throw std::invalid_argument("Denominator cannot be 0!\n");}
+        denominator_ = d;
+    }
+
+    [[nodiscard]] auto numerator() const
+    {
+        return numerator_;
+    }
+
+    void setNumerator(int n) noexcept
+    {
+        numerator_ = n;
+    }
+
+    [[nodiscard]] auto denominator() const noexcept
+    {
+        return denominator_;
+    }
+
+    void setDenominator(int d)
+    {
+        if(d == 0){throw std::invalid_argument("Denominator cannot be 0!\n");}
+        denominator_ = d;
+    }
+
+    Fraction& operator+(const Fraction&);
+    Fraction& operator*(const Fraction&);
+};
+#endif // FRACTION_H
---
+++ lab3/vector.cpp
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <stdexcept>
+#include <utility> // std::swap()
+
+#include "vector.h"
+
+#ifndef _MSC_FULL_VER // if not Visual Studio Compiler
+#else
+    #pragma message ("Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym")
+#endif
+
+Vector &Vector::operator=(const Vector &v){
+    if(this == &v)
+    {
+        return *this;
+    }
+
+    size_ = v.size_;
+    capacity_ = v.capacity_;
+
+    delete[] data_;
+
+    data_ = new Fraction[capacity_];
+    std::copy(v.data_, v.data_ + capacity_, data_);
+
+    return *this;
+}
+
+Fraction &Vector::operator[](std::size_t idx) {
+    if(idx >= size_)
+    {
+        throw std::out_of_range("Out of range!\n");
+    }
+    return data_[idx];
+}
+
+const Fraction &Vector::operator[](std::size_t idx) const {
+    if(idx >= size_)
+    {
+        throw std::out_of_range("Out of range!\n");
+    }
+    return data_[idx];
+}
+
+Vector &Vector::operator=(Vector &&v) noexcept {
+    if(this == &v)
+    {
+        return *this;
+    }
+
+    size_ = v.size_;
+    capacity_ = v.capacity_;
+
+    delete[] data_;
+
+    data_ = v.data_;
+
+    v.data_ = nullptr;
+    v.size_ = 0;
+    v.capacity_ = 0;
+
+    return *this;
+}
+
+void Vector::push_back(const Fraction &f) {
+    if(size_ + 1 > capacity_)
+    {
+        Fraction* new_data = new Fraction[capacity_ + 1];
+        std::copy(data_, data_ + capacity_, new_data);
+        new_data[capacity_] = f;
+
+        delete[] data_;
+
+        data_ = new_data;
+        size_++;
+        capacity_++;
+    }
+    else
+    {
+        data_[size_] = f;
+        size_++;
+    }
+}
