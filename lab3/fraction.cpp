cd158b1939a599f43064bada6c0d4c9b26bf6abf
MSSkowron <92554920+MSSkowron@users.noreply.github.com>
2022-04-10 21:56:22 +0200

    Add files via upload

---
+++ lab3/fraction.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdexcept> // std::out_of_range
+#include <numeric> // std::gcd/lcm
+
+#include "fraction.h"
+
+Fraction &Fraction::operator+(const Fraction &other)
+{
+    int lcm =  std::lcm(denominator_,other.denominator_);
+    int first = lcm / denominator_;
+    int second = lcm / other.denominator_;
+
+    numerator_ = (numerator_*first) + (other.numerator_*second);
+    denominator_ = lcm;
+
+    simplify();
+
+    return *this;
+}
+
+Fraction &Fraction::operator*(const Fraction &other)
+{
+    if(this == &other)
+    {
+        return *this;
+    }
+
+    numerator_ = numerator_ * other.numerator_;
+    denominator_ = denominator_ * other.denominator_;
+
+    simplify();
+
+    return *this;
+}
+
+void Fraction::simplify() {
+    int gcd = std::gcd(numerator_,denominator_);
+    numerator_ /= gcd;
+    denominator_ /= gcd;
+}
